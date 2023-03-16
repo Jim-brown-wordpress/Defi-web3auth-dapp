d609a8ee25d3d542c62fd57a0791e6f0fb1fe069
Jim-brown-wordpress <donose1122@gmail.com>
2023-03-16 17:51:46 -0400

    config setting

--- src/config.js
+++ src/config.js
@@ -1,24 +1,24 @@
 
 
-// export const networkConfig = {
-//     clientId: 'BIZCTpetrKrXgIb0vT5WKc8KsRRl2mYiIP3HkgX03WRrxnkL3ekqOYiiG0xC72i_hEUCVFlNUQ4m6N5F5VPg5zE',
-//     web3AuthNetwork: "testnet",
-//     chainConfig: {
-//       chainNamespace: 'eip155',
-//       chainId: "0x61",
-//       rpcTarget: "https://bsc-dataseed.binance.org/",
-//     }
-// }
-
 export const networkConfig = {
-  clientId: 'BIZCTpetrKrXgIb0vT5WKc8KsRRl2mYiIP3HkgX03WRrxnkL3ekqOYiiG0xC72i_hEUCVFlNUQ4m6N5F5VPg5zE',
-  web3AuthNetwork: "cyan",
-  chainConfig: {
-    chainNamespace: 'eip155',
-    chainId: "0x38",
-    rpcTarget: "https://rpc.ankr.com/bsc",
-  }
+    clientId: 'BIZCTpetrKrXgIb0vT5WKc8KsRRl2mYiIP3HkgX03WRrxnkL3ekqOYiiG0xC72i_hEUCVFlNUQ4m6N5F5VPg5zE',
+    web3AuthNetwork: "testnet",
+    chainConfig: {
+      chainNamespace: 'eip155',
+      chainId: "0x61",
+      rpcTarget: "https://bsc-dataseed.binance.org/",
+    }
 }
+
+// export const networkConfig = {
+//   clientId: 'BIZCTpetrKrXgIb0vT5WKc8KsRRl2mYiIP3HkgX03WRrxnkL3ekqOYiiG0xC72i_hEUCVFlNUQ4m6N5F5VPg5zE',
+//   web3AuthNetwork: "cyan",
+//   chainConfig: {
+//     chainNamespace: 'eip155',
+//     chainId: "0x38",
+//     rpcTarget: "https://rpc.ankr.com/bsc",
+//   }
+// }
 
 export const CONTRACT_REWARD_ADDRESS = '0xe6Cd6980834aA0Ea9F523Fb8B1Dd53f3BA9a8aef';
 export const CONTRACT_NFT_ADDRESS = '0x9Dcb659e354A83481FE8e44AF8CC4D8d1dC8bBb4';
